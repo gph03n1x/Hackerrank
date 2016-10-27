a275ba750f10e654586dfde4c0904ea6dcf18fab
Giannis Papaioannou <giannispapcod7@gmail.com>
2016-10-28 00:05:14 +0300

    Initial commit , added Bot_saves_princess.

---
+++ Artificial Intelligence/bot_saves_princess.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+void displayPathtoPrincess(int n, char grid[101][101]){
+    int mx = 0, my = 0, px = 0, py = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if (grid[i][j] == 'p') {
+                px = i;
+                py = j;
+            }
+            if (grid[i][j] == 'm') {
+                mx = i;
+                my = j;
+            }
+        }
+    }
+    if (my-py > 0){ 
+        for (int i=0; i<my-py; i++) {
+            printf("LEFT\n");
+        }
+    }
+    
+    if (py-my > 0){ 
+        for (int i=0; i<py-my; i++) {
+        printf("RIGHT\n");
+        }
+    }
+        
+    if (mx-px > 0){ 
+        for (int i=0; i<mx-px; i++) {
+        printf("UP\n");
+        }
+    }
+    
+    if (px-mx > 0){ 
+        for (int i=0; i<px-mx; i++) {
+        printf("DOWN\n");
+        }
+    }
+}
+int main(void) {
+
+    int m;
+    scanf("%d", &m);
+    char grid[101][101]={};
+    char line[101];
+
+    for(int i=0; i<m; i++) {
+        scanf("%s", line);
+        strcpy(grid[i], line);
+    }
+    displayPathtoPrincess(m,grid);
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## HackerRank
+
+This repository holds my solutions for various hackerrank problems.
+
+If you find any issues or suggestions with the solutions [open an issue](https://github.com/ehotinger/HackerRank/issues) and I'll check it out.
